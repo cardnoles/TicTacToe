ac715226576874e26e65ca69469fc671e40c3853
Sue Rasmussen <cardnoles10@gmail.com>
2015-12-16 00:42:40 -0500

    cleaned up a bit

--- tic-tac-toeTEST.js
+++ tic-tac-toeTEST.js
@@ -7,111 +7,17 @@ document.getElementById("game").onclick = playGame;
     board[i] = null;
     }
 
-  var gameOver = false;
-
-var playerX;
-var playerO;
-var firstPlayer;
-//var secondPlayer;
-var xIsFirst;
-var playerMove;
 
 
+  var playerX; //determined from input
+  var playerO; //determined from input
+  var firstPlayer; //determined from input
+  var xIsFirst; //help determine which player is first
+  var playerMove; //used to place the x or o in the box
+  var gameOver = false; //games stops when gameOver is true
 
-
-function playGame() {
-
-
-    playerX = document.getElementById("playerX").value;
-    //console.log("this is X " + playerX);
-    playerO = document.getElementById("playerO").value;
-    //console.log("this is O " + playerO);
-    document.querySelector("h2#x").textContent = playerX + " is Player X";
-    document.querySelector("h2#o").textContent = playerO + " is Player O";
-    firstPlayer = document.getElementById("playerOne").value;
-    //console.log(firstPlayer);
-    document.querySelector("h3#alerts").textContent = firstPlayer + " clicks first";
-    firstMove();
-}
-
-
-
-
-function gameWinner(gameBoard) {
-
-for (i = 0 ; i < gameBoard.length; i++) {
-    var Winner = true;
-    var xWinner = false;
-    var oWinner = false;
-    var xCounter = 0;
-    var oCounter = 0;
-
-
-
-
-
-    for (x = 0 ; x < gameBoard[i].length ; x++) {
-
-              if (gameBoard[i][x] === 'x') {
-              xCounter += 1;
-              //console.log("this is the xCounter: " + xCounter);
-               if(xCounter === 3 ){
-                //console.log("x is the winner");
-                xWinner = true;
-                gameOver = true;
-               }//closes if inside if
-             }
-
-
-
-              else if (gameBoard[i][x] === 'o') {
-              oCounter += 1;
-              console.log("this is the oCounter: " + oCounter);
-               if(oCounter === 3 ){
-                console.log("o is the winner");
-                oWinner = true;
-                gameOver = true;
-               }//closes if inside if
-             }//closes else if
-
-               else if (gameBoard[i][x] != 'x' && gameBoard[i][x] != 'o') {
-              //console.log("there is no chance");
-                Winner = false;
-             }//closes else if
-
-
-            else {
-              //console.log("no winner");
-              Winner = false;
-             }//closes else
-
-        }//closes second for loop
-
-
-        /*console.log("x won? " + xWinner);
-             //console.log("o won? " + oWinner);*/
-
-             if (xWinner){
-              document.querySelector("h3#alerts").textContent = playerX + " is the Winner!"
-              //alert(playerX + " is the Winner!");
-              }//closes if xWinner
-
-             else if (oWinner){
-              document.querySelector("h3#alerts").textContent = playerO + " is the Winner!"
-              //alert(playerO + " is the Winner");
-                }//closes if oWinner
-
-              /*else if (!Winner){
-                  alert("there is no winner");
-                }//closes else*/
-
-      } //closes first for loop
-
-
-} // gameWinner closes function
-
-
-  function firstMove() {
+//determine if x or o should be used first
+function firstMove() {
    if(firstPlayer === playerX){
         playerMove = 'x';
         xIsFirst = true;
@@ -121,222 +27,213 @@ for (i = 0 ; i < gameBoard.length; i++) {
     playerMove = 'o';
     xIsFirst = false;
       }
-      console.log("this is playerMove " + playerMove);
+   return playerMove;
 }
 
 
-var moveCounter = 0;
+
+//function to run the game
+function playGame() {
+    playerX = document.getElementById("playerX").value;
+    playerO = document.getElementById("playerO").value;
+    document.querySelector("h2#x").textContent = playerX + " is Player X";
+    document.querySelector("h2#o").textContent = playerO + " is Player O";
+    firstPlayer = document.getElementById("playerOne").value;
+    document.querySelector("h3#alerts").textContent = firstPlayer + " clicks first";
+    firstMove();
+}
+
+
+//function to determine the winner
+function gameWinner(gameBoard) {
+  //for loop runs through the array that contain arrays
+  for (i = 0 ; i < gameBoard.length; i++) {
+    var Winner = true;
+    var xWinner = false;
+    var oWinner = false;
+    var xCounter = 0;
+    var oCounter = 0;
+
+      //for loop runs through the inner arrays
+      for (x = 0 ; x < gameBoard[i].length ; x++) {
+              //checks to see if x has won
+              if (gameBoard[i][x] === 'x') {
+                 xCounter += 1;
+                   if(xCounter === 3 ){
+                       xWinner = true;
+                       gameOver = true;
+                   }//closes if inside if
+              }//closes check x if
+
+              //checks to see if i o has won
+              else if (gameBoard[i][x] === 'o') {
+                  oCounter += 1;
+                     if(oCounter === 3 ){
+                         oWinner = true;
+                         gameOver = true;
+                     }//closes if inside if
+              }//closes else if
+
+              else {
+                   Winner = false;
+              }//closes else
+      }//closes inner array loop line 58
+
+       //declares the winner
+    if (xWinner){
+              document.querySelector("h3#alerts").textContent = playerX + " is the Winner!"
+              }
+
+    else if (oWinner){
+              document.querySelector("h3#alerts").textContent = playerO + " is the Winner!"
+              }
+  } //closes first for loop line 50
+} // closes gameWinner function
 
 
 
+var moveCounter = 0; //counts the valid move clicks
+var divElements = document.getElementsByTagName("div"); //gets info for boxes
+var r; //random variable used in switch statement to fill the board array
 
-
-var divElements = document.getElementsByTagName("div");
-var r;
-
+//sets click listeners for the boxes
 for (i = 0 ; i < divElements.length ; i++) {
-divElements[i].onclick = function() {
-
-
+    divElements[i].onclick = function() {
+   //tells users if a box already has an x or an o
   if((this.querySelector("p").textContent === "X") || (this.querySelector("p").textContent === "O")) {
     document.querySelector("h3#alerts").textContent ='this box is occupied - choose again';
   }
-
-
+  //runs this if box is empty
   else {
+      //puts an X with some style in the box if it is X's turn
      if (playerMove === 'x') {
        this.querySelector("p").textContent = "X";
        this.querySelector("p").style.background = "white";
        this.querySelector("p").style.border = "5px solid gray";
        this.style.background = "black";
-  }
+       }
+     //puts an O with some style in the box if it is O's turn
      else {
        this.querySelector("p").textContent = "O";
        this.querySelector("p").style.background = "white";
        this.querySelector("p").style.border = "5px solid gray";
        this.querySelector("p").style.color = "red";
        this.style.background = "red";
-  }
+       }
+       //puts the id of the boxed clicked into the var boxNumber
   var boxNumber = this.id;
-  //console.log(boxNumber);
-  //console.log("this is board r: " + board[r]);
 
-switch(boxNumber) {
-  case "one":
-    r = 0;
-    if (playerMove === 'x') {
-      board[r] = 'x';
-     }
-    else {
-    board[r] = 'o';
-   }
-    break;
-  case "two":
-    r = 1;
-    if (playerMove === 'x') {
-      board[r] = 'x';
-     }
-    else {
-    board[r] = 'o';
-   }
-    break;
-  case "three":
-    r = 2;
-    if (playerMove === 'x') {
-      board[r] = 'x';
-     }
-    else {
-    board[r] = 'o';
-   }
-    break;
-  case "four":
-    r = 3;
-    if (playerMove === 'x') {
-      board[r] = 'x';
-     }
-    else {
-    board[r] = 'o';
-   }
-    break;
-  case "five":
-    r = 4;
-    if (playerMove === 'x') {
-      board[r] = 'x';
-     }
-    else {
-    board[r] = 'o';
-   };
-    break;
-  case "six":
-    r = 5;
-    if (playerMove === 'x') {
-      board[r] = 'x';
-     }
-    else {
-    board[r] = 'o';
-     }
-    break;
-  case "seven":
-    r = 6;
-    if (playerMove === 'x') {
-      board[r] = 'x';
-     }
-    else {
-    board[r] = 'o';
-     }
-    break;
-  case "eight":
-    r = 7;
-    if (playerMove === 'x') {
-      board[r] = 'x';
-     }
-    else {
-    board[r] = 'o';
-   }
-    break;
-  case "nine":
-    r = 8;
-    if (playerMove === 'x') {
-      board[r] = 'x';
-     }
-    else {
-    board[r] = 'o';
-   }
-    break;
-  default:
-    r = "you missed the box";
-    } //closes switch
-//console.log(r);
-//console.log(board[r]);
-console.log(board);
-//console.log("this is box 3 " + board[2]);
+  //checks to see which box was clicked and puts an x or o in board array index
+  switch(boxNumber) {
 
-var one = board[0];
-var two = board[1];
-var three = board[2];
-var four = board[3];
-var five = board[4];
-var six = board[5];
-var seven = board[6];
-var eight = board[7];
-var nine = board[8];
+    case "one":
+      r = 0;
+      playerMove === 'x' ? board[r] = 'x' : board[r] = 'o';
+      break;
+    case "two":
+      r = 1;
+      playerMove === 'x' ? board[r] = 'x' : board[r] = 'o';
+      break;
+    case "three":
+      r = 2;
+      playerMove === 'x' ? board[r] = 'x' : board[r] = 'o';
+      break;
+    case "four":
+      r = 3;
+      playerMove === 'x' ? board[r] = 'x' : board[r] = 'o';
+      break;
+    case "five":
+      r = 4;
+      playerMove === 'x' ? board[r] = 'x' : board[r] = 'o';
+      break;
+    case "six":
+      r = 5;
+      playerMove === 'x' ? board[r] = 'x' : board[r] = 'o';
+      break;
+    case "seven":
+      r = 6;
+      playerMove === 'x' ? board[r] = 'x' : board[r] = 'o';
+      break;
+    case "eight":
+      r = 7;
+      playerMove === 'x' ? board[r] = 'x' : board[r] = 'o';
+      break;
+    case "nine":
+      r = 8;
+      playerMove === 'x' ? board[r] = 'x' : board[r] = 'o';
+      break;
+    default:
+      r = "you missed the box";
+  } //closes switch
 
-//create row arrays
-var gameBoardRows = [[one, two, three],[four, five, six],[seven, eight, nine]];
+  // puts values from switch in variables to go into arrays
+  // probably could have put board[] in arrays, but this looks better to my eye
+  var one = board[0];
+  var two = board[1];
+  var three = board[2];
+  var four = board[3];
+  var five = board[4];
+  var six = board[5];
+  var seven = board[6];
+  var eight = board[7];
+  var nine = board[8];
 
-//create column arrays
-var gameBoardColumns = [[one, four, seven],[two, five, eight],[three, six, nine]];
+  //create row arrays
+  var gameBoardRows = [[one, two, three],[four, five, six],[seven, eight, nine]];
 
-//create diagonal arrays
-var gameBoardDiagonals = [[one, five, nine],[three, five, seven]];
+  //create column arrays
+  var gameBoardColumns = [[one, four, seven],[two, five, eight],[three, six, nine]];
 
-//console.log(gameBoardRows);
+  //create diagonal arrays
+  var gameBoardDiagonals = [[one, five, nine],[three, five, seven]];
 
-gameWinner(gameBoardRows);
-gameWinner(gameBoardColumns);
-gameWinner(gameBoardDiagonals);
+  //runs the gameWinner function to see if any of the arrays contain a winning combo
+  gameWinner(gameBoardRows);
+  gameWinner(gameBoardColumns);
+  gameWinner(gameBoardDiagonals);
 
-
-
-
-
-if (!gameOver) {
-moveCounter += 1;
-
-console.log(moveCounter);
-  if(xIsFirst) {
-
-       if (moveCounter % 2 != 0) {
+  //still in click function increments moveCounter if game is not over
+  if (!gameOver) {
+     moveCounter += 1;
+         //sets player move label to x for even moves, o for odd if x player goes first
+      if(xIsFirst) {
+         if (moveCounter % 2 != 0) {
          playerMove = "o";
          document.querySelector("h3#alerts").textContent = "it is " + playerO +"'s turn";
-         //alert("it is " + playerO +"'s turn");
-       }
-       else {
-          playerMove = "x";
-          document.querySelector("h3#alerts").textContent = "it is " + playerX +"'s turn";
-          //alert("it is " +  playerX +"'s turn");
-       }
-  }
-  else {
-       if (moveCounter % 2 != 0) {
+         }
+         else {
          playerMove = "x";
          document.querySelector("h3#alerts").textContent = "it is " + playerX +"'s turn";
-         //alert("it is " +  playerX +"'s turn");
-       }
-       else {
-          playerMove = "o";
-          document.querySelector("h3#alerts").textContent = "it is " + playerO +"'s turn";
-          //alert("it is " + playerO +"'s turn");
-       }
-  }
-  if (moveCounter === 9) {
-    document.querySelector("h3#alerts").textContent = "cat's tail - no winner";
-  }
-} //close gameOver if
-else {
-  console.log("announce winner and reset game");
-}
-
-
-     }//closes else on 133
+         }
+      }
+      //sets player move label to o for even moves, x for odd if o player goes first
+      else {
+         if (moveCounter % 2 != 0) {
+         playerMove = "x";
+         document.querySelector("h3#alerts").textContent = "it is " + playerX +"'s turn";
+         }
+         else {
+         playerMove = "o";
+         document.querySelector("h3#alerts").textContent = "it is " + playerO +"'s turn";
+         }
+      }
+      //declares a cat's tail game if move counter gets to 9 without a winner
+      if (moveCounter === 9) {
+      document.querySelector("h3#alerts").textContent = "cat's tail - no winner";
+      }
+    } //close gameOver if line 196
+   } //closes else on 107
   } //closes onclick function
-
 } //closes for
 
 
-
-//playGame(); //line 18
-//firstMove(); // line 99
-
-
-
-//this function is ridiculous
 function playAgain() {
 
     location.reload();
+}
+  document.getElementById("again").onclick = playAgain;
 
-
-
+//this function is ridiculous
   /*console.log("playing again");
 
   document.getElementById("textOne").textContent = "";
@@ -398,8 +295,7 @@ function playAgain() {
     moveCounter = 0;
 }*/
 
-}
-  document.getElementById("again").onclick = playAgain;
+
 
   //playAgain();
 
